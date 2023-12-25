ec2e07f24e7f82fee83251f54dfc28c644550717
Ayush Gupta <127961367+AyusGup@users.noreply.github.com>
2023-12-25 14:31:42 +0530

    Update route.ts

--- server/src/app/api/auth/[...nextauth]/route.ts
+++ server/src/app/api/auth/[...nextauth]/route.ts
@@ -6,8 +6,8 @@ export const authOptions = {
   secret: process.env.NEXTAUTH_SECRET,
   providers: [
     GoogleProvider({
-      clientId: "1010618076889-08tjso93glf22tik2okuvcic621u0l8k.apps.googleusercontent.com",
-      clientSecret: "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",
+      clientId: ,
+      clientSecret: ,
     }),
     // ...add more providers here
   ],
